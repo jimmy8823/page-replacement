951bf439dee723b6eb67314d1210711026f280e9
jimmy8823 <jimmy0844@mail.fcu.edu.tw>
2022-10-11 00:37:12 +0800

    10/10

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from generator_refstring import generator_refstring_locality,generator_refstring_random;
-from page_replacement import FIFO;
+from page_replacement import FIFO, OPTIMAL;
 
 def main():
     generator_refstring_random();
     generator_refstring_locality();
-    for i in range(1,10):
-        print("frame size: " + str(i));
-        FIFO("test/random.txt",i*10);
-
+    """for i in range(1,11): # simulate FIFO with random test in memory size 10 20 30 40....100
+        print("(random test)frame size: " + str(i*10));
+        OPTIMAL("test/random.txt",i*10);"""
+    print("(locality test)frame size: " + str(100));
+    FIFO("test/locality.txt",100);
 main()
--- test.py
+++ test.py
@@ -8,14 +8,17 @@ def FIFO(test_file , frame_size):
     p_memory_dirty_bit = queue.Queue(frame_size);# init variable
     f = open(test_file,'r');
     list = f.readlines(); # load test file to list
-    for i in list:  
+    for i in list: 
         ref_s = i[0:i.find(' ')];
         dirty_bit = i[i.find(' ')+1];
+        print(ref_s + " " + dirty_bit);
         if ref_s in p_memory_ref_s.queue: # if ref string in memory
+            print("hit !");
             continue;
         if(p_memory_ref_s.full()): # (page fault occurs) need to select victim
             tmp = p_memory_dirty_bit.get()
-            if int(tmp)==1: # if dirty bit is set
+            print("page fault select a victim")
+            if tmp == '1': # if dirty bit is set
                 write_back+=1;
                 interrupt+=1;
             p_memory_ref_s.get(); # remove element from head
@@ -24,6 +27,7 @@ def FIFO(test_file , frame_size):
         else:# frame have space but ref isnt in memory (page fault occurs)
             p_memory_ref_s.put(ref_s);
             p_memory_dirty_bit.put(dirty_bit);
+            print("page fault load into frame")
         page_fault+=1;
         interrupt+=1;
     print(str(page_fault) + " " +str(interrupt) + " " + str(write_back));
@@ -41,7 +45,6 @@ def OPTIMAL(test_file , frame_size):
         item = list[i];
         ref_s = item[0:item.find(' ')];
         dirty_bit = item[item.find(' ')+1];
-        hit = 0;
         if ref_s in p_memory_ref_s: # check frame to see exist ref page or not if the page exist in frame doesnt need to replace 
             continue;
         else: # page fault occurs
@@ -54,25 +57,28 @@ def OPTIMAL(test_file , frame_size):
                 longest_period = 0;
                 for k in range(0,len(p_memory_ref_s)):
                     period = 0;
+                    flag = 0;
+                    print(p_memory_ref_s[k])
                     for l in range(current_index+1,len(list)):
                         period+=1;
                         tmp=list[l];
-                        if(p_memory_ref_s[k]==tmp[0:tmp.find(" ")]):
+                        if(str(p_memory_ref_s[k])==tmp[0:tmp.find(" ")]):
+                            flag=1;
                             break;
-                    if(period==len(list)-current_index+1):# isn`t exist (must be the longest not used)
+                    if(period==len(list)-(current_index+1) and flag==0):# isn`t exist (must be the longest not used)
                         longest_not_used_page_index = k;
                         break;
                     if(period>longest_period):
                         longest_period = period;
                         longest_not_used_page_index = k;
-                if(int(p_memory_dirty_bit[longest_not_used_page_index])==1):# victim dirty bit is set
+                if(p_memory_dirty_bit[longest_not_used_page_index]=='1'):# victim dirty bit is set
                     write_back+=1;
                     interrupt+=1;
-                p_memory_ref_s[longest_not_used_page_index] = ref_s;
                 p_memory_dirty_bit[longest_not_used_page_index] = dirty_bit;
             page_fault+=1;
             interrupt+=1;
     print(str(page_fault) + " " +str(interrupt) + " " + str(write_back));
     f.close();
 
-#def ESC(test_file,frame_size):
+OPTIMAL("test.txt",5);
+#FIFO("test.txt",5);
